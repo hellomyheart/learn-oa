194188d9915d212f4e35b2f5e356aaf65112dce1
stephen shen <723809534@qq.com>
2020-09-25 19:01:09 +0800

    加入Spring配置

---
+++ learn-oa-web-api/learn-oa-web-teacher-api/src/main/java/cn/hellomyheart/learn/oa/teacher/converter/CustomerDateConvert.java
@@ -0,0 +1,32 @@
+package cn.hellomyheart.learn.oa.teacher.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CustomerDateConvert implements Converter<String, Date> {
+    private SimpleDateFormat[] sdfs=new SimpleDateFormat[]{
+            new SimpleDateFormat ("yyyy-MM-dd"),
+            new SimpleDateFormat ("yyyyMMdd"),
+            new SimpleDateFormat ("yyyy年MM月dd日"),
+            new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss")
+    };
+
+    @Override
+    public Date convert (String s) {
+        if(s==null||s.isEmpty ()){
+            return null;
+        }
+        for (SimpleDateFormat sdf : sdfs) {
+            try {
+                return sdf.parse (s);
+            } catch (ParseException e) {
+                e.printStackTrace ();
+                continue;
+            }
+        }
+        return null;
+    }
+}
